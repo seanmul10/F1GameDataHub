5c1bc00e81c27f746e123596a631a98957b0df2e
seanmul10 <seanmul10@gmail.com>
2025-06-15 13:37:01 +0100

    feat: Add packet recording
    Adds the simple packet recording tool which records the timing and byte buffer of received udp packets and stores them in a .gz file for the purpose of replaying later.

--- F1GameDataHub/PacketRecording/Program.cs
+++ F1GameDataHub/PacketRecording/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Instantiate the recorder, and start it
+using PacketRecording;
+using System.Net;
+
+var recorder = new UdpPacketRecorder(new IPEndPoint(IPAddress.Loopback, 20777), @"D:\Temp\test_dump");
+var cts = new CancellationTokenSource();
+
+await Task.Run(async () =>
+{
+    await recorder.StartRecording(compressionEnabled: false, cts.Token);
+});
+
+Console.WriteLine("Press Enter to stop recording...");
+Console.ReadLine();
+
+cts.Cancel();
