9e0957b9b0dbd2336d0310d52aaf8b759d567539
seanmul10 <seanmul10@gmail.com>
2025-06-19 23:36:28 +0100

    feat: Add IF1Packet interface
    All packet structs will inherit from this since they will all have a header property available.

---
+++ F1GameDataHub/F1Packets/F125/IF1Packet.cs
@@ -0,0 +1,14 @@
+﻿using F1.Common;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace F1Packets.F125
+{
+    public interface IF1Packet
+    {
+        /// <summary>
+        /// The header of the packet containing metadata of the packet and game session.
+        /// </summary>
+        PacketHeader Header { get; }
+    }
+}
