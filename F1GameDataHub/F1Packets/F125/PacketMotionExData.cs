9763f5d3c8d884d3a0037a394441bcfdf66481f6
seanmul10 <seanmul10@gmail.com>
2025-06-21 00:00:12 +0100

    Refactor F125 packet structs to readonly with properties
    Converted PacketMotionExData, TyreSetData, and PacketTyreSetsData structs to readonly and replaced public fields with private readonly fields and public properties. Added XML documentation comments for all properties to improve code clarity and maintainability. Implemented IF1Packet interface where appropriate.

--- F1GameDataHub/F1Packets/F125/PacketMotionExData.cs
+++ F1GameDataHub/F1Packets/F125/PacketMotionExData.cs
@@ -4,48 +4,143 @@ using System.Runtime.InteropServices;
 namespace F1Packets.F125
 {
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct PacketMotionExData
+    public readonly struct PacketMotionExData : IF1Packet
     {
-        public PacketHeader Header;
+        private readonly PacketHeader _header;
+        private readonly TyreDataBuffer<float> _suspensionPosition;
+        private readonly TyreDataBuffer<float> _suspensionVelocity;
+        private readonly TyreDataBuffer<float> _suspensionAcceleration;
+        private readonly TyreDataBuffer<float> _wheelSpeed;
+        private readonly TyreDataBuffer<float> _wheelSlipRatio;
+        private readonly TyreDataBuffer<float> _wheelSlipAngle;
+        private readonly TyreDataBuffer<float> _wheelLatForce;
+        private readonly TyreDataBuffer<float> _wheelLongForce;
+        private readonly float _heightOfCOGAboveGround;
+        private readonly Vector3<float> _localVelocity;
+        private readonly Vector3<float> _angularVelocity;
+        private readonly Vector3<float> _angularAcceleration;
+        private readonly float _frontWheelsAngle;
+        private readonly TyreDataBuffer<float> _wheelVertForce;
+        private readonly float _frontAeroHeight;
+        private readonly float _rearAeroHeight;
+        private readonly float _frontRollAngle;
+        private readonly float _rearRollAngle;
+        private readonly float _chassisYaw;
+        private readonly float _chassisPitch;
+        private readonly TyreDataBuffer<float> _wheelCamber;
+        private readonly TyreDataBuffer<float> _wheelCamberGain;
 
-        // Arrays for 4 wheels in order: RL, RR, FL, FR
-        public TyreDataBuffer<float> SuspensionPosition;
-        public TyreDataBuffer<float> SuspensionVelocity;
-        public TyreDataBuffer<float> SuspensionAcceleration;
-        public TyreDataBuffer<float> WheelSpeed;
-        public TyreDataBuffer<float> WheelSlipRatio;
-        public TyreDataBuffer<float> WheelSlipAngle;
-        public TyreDataBuffer<float> WheelLatForce;
-        public TyreDataBuffer<float> WheelLongForce;
+        /// <inheritdoc/>
+        public PacketHeader Header => _header;
 
-        public float HeightOfCOGAboveGround;
+        /// <summary>
+        /// The position of the suspension for each wheel in meters.
+        /// </summary>
+        public TyreDataBuffer<float> SuspensionPosition => _suspensionPosition;
 
-        public float LocalVelocityX;
-        public float LocalVelocityY;
-        public float LocalVelocityZ;
+        /// <summary>
+        /// The velocity of the suspension for each wheel in meters per second.
+        /// </summary>
+        public TyreDataBuffer<float> SuspensionVelocity => _suspensionVelocity;
 
-        public float AngularVelocityX;
-        public float AngularVelocityY;
-        public float AngularVelocityZ;
+        /// <summary>
+        /// The acceleration of the suspension for each wheel in meters per second squared.
+        /// </summary>
+        public TyreDataBuffer<float> SuspensionAcceleration => _suspensionAcceleration;
 
-        public float AngularAccelerationX;
-        public float AngularAccelerationY;
-        public float AngularAccelerationZ;
+        /// <summary>
+        /// The speed of each wheel in meters per second.
+        /// </summary>
+        public TyreDataBuffer<float> WheelSpeed => _wheelSpeed;
 
-        public float FrontWheelsAngle;
+        /// <summary>
+        /// The wheel slip ratio for each wheel, representing the difference between the wheel speed and the vehicle speed.
+        /// </summary>
+        public TyreDataBuffer<float> WheelSlipRatio => _wheelSlipRatio;
 
-        public TyreDataBuffer<float> WheelVertForce;
+        /// <summary>
+        /// The wheel slip angle for each wheel in radians, indicating the angle between the wheel's direction and its actual path.
+        /// </summary>
+        public TyreDataBuffer<float> WheelSlipAngle => _wheelSlipAngle;
 
-        public float FrontAeroHeight;
-        public float RearAeroHeight;
+        /// <summary>
+        /// The lateral force on each wheel in newtons.
+        /// </summary>
+        public TyreDataBuffer<float> WheelLatForce => _wheelLatForce;
 
-        public float FrontRollAngle;
-        public float RearRollAngle;
+        /// <summary>
+        /// The longitudinal force on each wheel in newtons.
+        /// </summary>
+        public TyreDataBuffer<float> WheelLongForce => _wheelLongForce;
 
-        public float ChassisYaw;
-        public float ChassisPitch;
+        /// <summary>
+        /// The height of the center of gravity above the ground in meters.
+        /// </summary>
+        public float HeightOfCOGAboveGround => _heightOfCOGAboveGround;
 
-        public TyreDataBuffer<float> WheelCamber;
-        public TyreDataBuffer<float> WheelCamberGain;
+        /// <summary>
+        /// The velocity of the car in local space in meters per second.
+        /// </summary>
+        public Vector3<float> LocalVelocity => _localVelocity;
+
+        /// <summary>
+        /// The angular velocity of the car in radians per second.
+        /// </summary>
+        public Vector3<float> AngularVelocity => _angularVelocity;
+
+        /// <summary>
+        /// The angular acceleration of the car in radians per second squared.
+        /// </summary>
+        public Vector3<float> AngularAcceleration => _angularAcceleration;
+
+        /// <summary>
+        /// Gets the current angle of the front wheels in radians.
+        /// </summary>
+        public float FrontWheelsAngle => _frontWheelsAngle;
+
+        /// <summary>
+        /// The vertical force on each wheel in newtons.
+        /// </summary>
+        public TyreDataBuffer<float> WheelVertForce => _wheelVertForce;
+
+        /// <summary>
+        /// The height of the front plank edge above the ground in meters.
+        /// </summary>
+        public float FrontAeroHeight => _frontAeroHeight;
+
+        /// <summary>
+        /// The height of the rear plank edge above the ground in meters.
+        /// </summary>
+        public float RearAeroHeight => _rearAeroHeight;
+
+        /// <summary>
+        /// The roll angle of the front suspension in radians.
+        /// </summary>
+        public float FrontRollAngle => _frontRollAngle;
+
+        /// <summary>
+        /// The roll angle of the rear suspension in radians.
+        /// </summary>
+        public float RearRollAngle => _rearRollAngle;
+
+        /// <summary>
+        /// The yaw angle of the chassis relative to the direction of travel in radians.
+        /// </summary>
+        public float ChassisYaw => _chassisYaw;
+
+        /// <summary>
+        /// The pich angle of the chassis relative to the direction of travel in radians.
+        /// </summary>
+        public float ChassisPitch => _chassisPitch;
+
+        /// <summary>
+        /// The camber of each wheel in radians.
+        /// </summary>
+        public TyreDataBuffer<float> WheelCamber => _wheelCamber;
+
+        /// <summary>
+        /// The camber gain of each wheel in radians, representing the difference between the active camber and dynamic camber.
+        /// </summary>
+        public TyreDataBuffer<float> WheelCamberGain => _wheelCamberGain;
     }
 }
--- F1GameDataHub/F1Packets/F125/PacketTyreSetsData.cs
+++ F1GameDataHub/F1Packets/F125/PacketTyreSetsData.cs
@@ -4,17 +4,63 @@ using System.Runtime.InteropServices;
 namespace F1Packets.F125
 {
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct TyreSetData
+    public readonly struct TyreSetData
     {
-        public byte ActualTyreCompound;
-        public byte VisualTyreCompound;
-        public byte Wear;
-        public byte Available;
-        public byte RecommendedSession;
-        public byte LifeSpan;
-        public byte UsableLife;
-        public short LapDeltaTime;
-        public byte Fitted;
+        private readonly byte _actualTyreCompound;
+        private readonly byte _visualTyreCompound;
+        private readonly byte _wear;
+        private readonly byte _available;
+        private readonly byte _recommendedSession;
+        private readonly byte _lifeSpan;
+        private readonly byte _usableLife;
+        private readonly short _lapDeltaTime;
+        private readonly byte _fitted;
+
+        /// <summary>
+        /// The actual tyre compound of this set.
+        /// </summary>
+        public byte ActualTyreCompound => _actualTyreCompound;
+
+        /// <summary>
+        /// The visual tyre compound of this set.
+        /// </summary>
+        public byte VisualTyreCompound => _visualTyreCompound;
+
+        /// <summary>
+        /// The percentage of wear on this set.
+        /// </summary>
+        public byte Wear => _wear;
+
+        /// <summary>
+        /// Whether this tyre set is available for use.
+        /// </summary>
+        public bool Available => _available != 0;
+
+        /// <summary>
+        /// The recommended session for this tyre set.
+        /// See documentation for the possible values.
+        /// </summary>
+        public byte RecommendedSession => _recommendedSession;
+
+        /// <summary>
+        /// The remaining number of laps this tyre set is expected to last.
+        /// </summary>
+        public byte LifeSpan => _lifeSpan;
+
+        /// <summary>
+        /// The number of laps recommended for this compound.
+        /// </summary>
+        public byte UsableLife => _usableLife;
+
+        /// <summary>
+        /// The delta time in milliseconds for this tyre set compared to the fitted tyre set.
+        /// </summary>
+        public short LapDeltaTime => _lapDeltaTime;
+
+        /// <summary>
+        /// Whether this tyre set is currently fitted to the car.
+        /// </summary>
+        public bool Fitted => _fitted != 0;
     }
 
     [InlineArray(PacketConstants.MaxNumTyreSets)]
@@ -24,11 +70,29 @@ namespace F1Packets.F125
     }
 
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct PacketTyreSetsData
+    public readonly struct PacketTyreSetsData : IF1Packet
     {
-        public PacketHeader Header;
-        public byte CarIdx;
-        public TyreSetDataBuffer TyreSetData;
-        public byte FittedIdx;
+        private readonly PacketHeader _header;
+        private readonly byte _carIdx;
+        private readonly TyreSetDataBuffer _tyreSetData;
+        private readonly byte _fittedIdx;
+
+        /// <inheritdoc/>
+        public PacketHeader Header => _header;
+
+        /// <summary>
+        /// The index of the car this data is for.
+        /// </summary>
+        public byte CarIdx => _carIdx;
+
+        /// <summary>
+        /// The data for each tyre set allocated to this car.
+        /// </summary>
+        public TyreSetDataBuffer TyreSetData => _tyreSetData;
+
+        /// <summary>
+        /// The index of the tyre set currently fitted to the car.
+        /// </summary>
+        public byte FittedIdx => _fittedIdx;
     }
 }
