a6694146d956786aa1b1a49553f645c402182ffd
seanmul10 <seanmul10@gmail.com>
2025-07-26 12:45:07 +0100

    feat: Add static data tables
    Added idempotent table creation and population for necessary static data.

---
+++ Scripts/Schema/PenaltyTypes.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS PenaltyTypes (
+    id INTEGER PRIMARY KEY,
+    type TEXT NOT NULL
+);
---
+++ Scripts/Schema/Tracks.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS Tracks (
+    id INTEGER PRIMARY KEY,
+    type TEXT NOT NULL
+);
